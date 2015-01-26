5a6cb6b43a57c8c8b9c4ae5d9e2597644d4b8aaf
KerstenDoering <kersten.doering@pharmazie.uni-freiburg.de>
2015-01-26 18:38:26 +0100

    Modified version only indexing PubMed titles and abstracts

---
+++ full_text_index_title_text/Article.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+    Copyright (c) 2014, Kersten Doering <kersten.doering@gmail.com>, Christian Senger <der.senger@googlemail.com>
+"""
+
+import xappy
+import sys
+
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from SynonymParser import SynonymParser
+
+class Article():
+    
+    user          = "parser"
+    password      = "parser"
+    host          = "localhost"
+    port          = "5432"
+    db            = ""
+    con           = "postgresql://"+user+":"+password+"@"+host+":"+port+"/"
+
+    #Kersten: set these attributes when calling static function getConnection(database)
+    base          = None#declarative_base()
+    engine        = None#create_engine(__con, pool_recycle = 900, echo=False)
+    base          = None#__base.metadata.create_all(__engine)
+    session       = None#sessionmaker(bind=__engine)()    
+
+    __count         = 0
+    __countMsg      = ""
+
+    def __init__(
+                 self,
+                 pmid
+                 ):
+
+        self.__pmid     = int(pmid)
+        self.__title    = None
+        self.__abstract = None
+        # not used in title_text version
+#        self.__chemicals= []
+#        self.__keywords = []
+#        self.__mesh     = []
+        
+        self.__loadStub()
+        # not used in title_text version
+#        self.__loadChemicals()
+#        self.__loadKeywords()
+#        self.__loadMeSH()
+    
+        Article.__count     += 1
+        nbs                 = len(Article.__countMsg)        
+        Article.__countMsg  = "article %s created" % (str(Article.__count))
+        sys.stdout.write('\b' * nbs + Article.__countMsg)
+        
+    @staticmethod
+    def getConnection(database):
+        
+        Article.con           = "postgresql://"+Article.user+":"+Article.password+"@"+Article.host+":"+Article.port+"/"+database
+        Article.base          = declarative_base()
+        Article.engine        = create_engine(Article.con, pool_recycle = 900, echo=False)
+        Article.base          = Article.base.metadata.create_all(Article.engine)
+        Article.session       = sessionmaker(bind=Article.engine)()
+        
+
+    def getPMID(self):
+        return self.__pmid
+    
+    def getTitle(self):
+        return self.__title
+    
+    def getAbstract(self):
+        return self.__abstract
+    
+    # not used in title_text version
+#    def getChemicals(self):
+#        return self.__chemicals
+#    
+#    def getKeywords(self):
+#        return self.__keywords
+
+#    def getMeSH(self):
+#        return self.__mesh
+
+    def __loadStub(self):
+            pmid = str(self.__pmid)
+            #print "####",pmid,"####"#in this case it is always one pmid - it is not a "complete" join
+            stmt = """
+            SELECT 
+                pmid,
+                article_title as title,
+                abstract_text as abstract
+            FROM 
+                pubmed.tbl_medline_citation
+                    LEFT OUTER JOIN
+                pubmed.tbl_abstract
+                        ON pmid = fk_pmid
+            WHERE
+                pmid = '"""+pmid+"""'
+            ;
+            """
+        
+            articles = Article.session.query(
+                    "pmid",
+                    "title",
+                    "abstract"
+            ).from_statement(stmt)
+            
+            for article in articles:
+                self.__title    = article.title
+                self.__abstract = article.abstract
+                break;
+
+# not used in title_text version
+#    def __loadChemicals(self):
+#        pmid = str(self.__pmid)
+#        
+#        stmt = """
+#            SELECT 
+#                name_of_substance AS substance
+#            FROM 
+#                pubmed.tbl_chemical
+#            WHERE
+#                fk_pmid = '"""+pmid+"""'
+#            ORDER BY 
+#                name_of_substance;
+#        """    
+
+#        
+#        substances = Article.session.query(
+#                    "substance"
+#        ).from_statement(stmt)
+#        
+#        for substance in substances:
+#            self.__chemicals.append(substance.substance)
+
+#    def __loadKeywords(self):
+#        pmid = str(self.__pmid)
+#        
+#        stmt = """
+#            SELECT 
+#                keyword
+#            FROM 
+#                pubmed.tbl_keyword
+#            WHERE
+#                fk_pmid = '"""+pmid+"""'
+#            ORDER BY 
+#                keyword;
+#        """    
+
+#        
+#        keywords = Article.session.query(
+#                    "keyword"
+#        ).from_statement(stmt)
+#        
+#        for keyword in keywords:
+#            self.__keywords.append(keyword.keyword)
+
+#    def __loadMeSH(self):
+#        pmid = str(self.__pmid)
+#        
+#        stmt = """
+#            SELECT 
+#                descriptor_name
+#            FROM 
+#                pubmed.tbl_mesh_heading
+#            WHERE
+#                fk_pmid = '"""+pmid+"""'
+#            ORDER BY 
+#                descriptor_name;
+#        """    
+
+#        
+#        mesh_terms = Article.session.query(
+#                    "descriptor_name"
+#        ).from_statement(stmt)
+#        
+#        for descriptor_name in mesh_terms:
+#            self.__mesh.append(descriptor_name.descriptor_name)
+
+    @staticmethod
+    def getArticlesByYear(b_year, e_year):
+        b_year            = int(b_year)
+        e_year            = int(e_year)
+        
+        
+        stmt = """
+            SELECT 
+                pmc.fk_pmid
+            FROM 
+                pubmed.tbl_journal pmc
+            WHERE
+                pub_date_year >= """+str(b_year)+"""
+                    AND
+                pub_date_year <= """+str(e_year)+"""
+           
+        ;
+        """ 
+
+        pmids = Article.session.query(
+                    "fk_pmid"
+        ).from_statement(stmt)
+        return [Article(pmid.fk_pmid) for pmid in pmids]
+
+    
+    @staticmethod
+    def closeConnection():
+        Article.session.close()
+
---
+++ full_text_index_title_text/PubMedXapian.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+    Copyright (c) 2014, Kersten Doering <kersten.doering@gmail.com>, Christian Senger <der.senger@googlemail.com>
+"""
+
+import xappy
+import sys
+import os
+
+from SynonymParser import SynonymParser
+from Article import Article
+
+class PubMedXapian():
+    __indexCount  = 0
+    __indexMsg    = ""
+
+    def __init__(   self,
+                    directory_name,
+                    #no absolut path
+                    xapianPath = "xapian",
+                 ):
+        self.__xapianPath   = os.path.join( xapianPath, directory_name )
+        self.__pmids        = []
+        self.__searchConn   = None 
+
+    def __buildDoc(self, article):
+        if article.getTitle() == None: return None
+        
+        doc = xappy.UnprocessedDocument()
+        doc.fields.append(xappy.Field("title", article.getTitle()))
+        if article.getAbstract() == None:
+            pass
+        else:
+            doc.fields.append(xappy.Field("text", article.getAbstract()))
+        
+        # not used in title_text version
+#        #'INDEX_EXACT' - maximum length 220, but prefix "XA" is added to each term in the document 
+#        #maximum length 218
+#        for chemical in [chemical for chemical in article.getChemicals() if len(chemical) < 219]:
+#            doc.fields.append(xappy.Field("chemical_exact", chemical))
+
+#        for keyword in article.getKeywords():
+#            doc.fields.append(xappy.Field("keyword", keyword))
+
+#        for mesh in article.getMeSH():
+#            doc.fields.append(xappy.Field("mesh", mesh))
+
+        doc.id = str(article.getPMID())
+        return doc
+
+    def buildIndexWithArticles(self, articles):
+        conn = xappy.IndexerConnection(self.__xapianPath)
+
+        #add priority to title field in case of ranked matching (weight=5)- index all fields and store data
+        conn.add_field_action('title', xappy.FieldActions.INDEX_FREETEXT, weight=5, language='en')
+        conn.add_field_action('text', xappy.FieldActions.INDEX_FREETEXT, language='en')
+        # not used in title_text version
+#        conn.add_field_action('chemical_exact', xappy.FieldActions.INDEX_EXACT)
+#        conn.add_field_action('keyword', xappy.FieldActions.INDEX_FREETEXT, language='en')
+#        conn.add_field_action('mesh', xappy.FieldActions.INDEX_FREETEXT, language='en')
+
+        conn.add_field_action('text', xappy.FieldActions.STORE_CONTENT)
+        conn.add_field_action('title', xappy.FieldActions.STORE_CONTENT)
+        # not used in title_text version
+#        conn.add_field_action('chemical_exact', xappy.FieldActions.STORE_CONTENT)
+#        conn.add_field_action('keyword', xappy.FieldActions.STORE_CONTENT)
+#        conn.add_field_action('mesh', xappy.FieldActions.STORE_CONTENT)
+
+        for article in articles:
+            doc = self.__buildDoc(article)
+            if doc == None: continue
+            try:
+                #process doc to pdoc explicitly - not needed here
+                #pdoc = conn.process(doc)
+                conn.add(doc)
+            except:
+                continue
+
+            PubMedXapian.__indexCount += 1
+            nbs = len(PubMedXapian.__indexMsg)
+            PubMedXapian.__indexMsg  = "article %s indexed" % (str(PubMedXapian.__indexCount))
+            sys.stdout.write('\b' * nbs + PubMedXapian.__indexMsg)
+        conn.flush()
+        conn.close()
+
+    def findPMIDsWithSynonyms(self, synonyms):
+        if self.__searchConn == None:
+            self.__searchConn = xappy.SearchConnection(self.__xapianPath)
+            self.__searchConn.reopen()
+
+        xapian_querys = []
+
+        for querystring in synonyms:
+            # not used in title_text version
+#            title, text, keyword, chemical_exact, mesh = '"' + querystring + '"', '"' + querystring + '"', '"' + querystring + '"', '"' + querystring + '"', '"' + querystring + '"'
+            title, text = '"' + querystring + '"', '"' + querystring + '"'
+
+
+            xapian_querys.append( self.__searchConn.query_field('title', title) )
+            xapian_querys.append( self.__searchConn.query_field('text', text) )
+            # not used in title_text version
+#            xapian_querys.append( self.__searchConn.query_field('keyword', keyword) )
+#            xapian_querys.append( self.__searchConn.query_field('chemical_exact', chemical_exact) )
+#            xapian_querys.append( self.__searchConn.query_field('mesh', mesh) )
+
+        merged_q = self.__searchConn.query_composite(self.__searchConn.OP_OR, xapian_querys)
+        results=self.__searchConn.search(merged_q, 0, self.__searchConn.get_doccount())
+
+        return [r.id for r in results] 
+
